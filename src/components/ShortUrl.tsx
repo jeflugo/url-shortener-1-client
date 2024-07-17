bd4d14ad8a3ab539366965b698da809dd7342f73
jeflugo <jefersoneduardoluego@gmail.com>
2024-07-16 23:15:13 -0400

    add react-copy-to-clipboard

--- src/components/ShortUrl.tsx
+++ src/components/ShortUrl.tsx
@@ -1,4 +1,6 @@
 import { BiCopy } from 'react-icons/bi'
+import { CopyToClipboard } from 'react-copy-to-clipboard'
+import toast from 'react-hot-toast'
 
 type ShortUrlProps = {
 	shortUrl: string
@@ -9,9 +11,14 @@ export default function ShortUrl({ shortUrl }: ShortUrlProps) {
 		<div className='text-center mt-4 flex flex-col items-center'>
 			<h3 className='text-2xl font-medium'>Short Url:</h3>
 			<p className='mb-2'>{shortUrl}</p>
-			<button className='flex items-center gap-2 rounded bg-blue-600 text-white py-1 px-2'>
-				<span>Copy url</span> <BiCopy />
-			</button>
+			<CopyToClipboard
+				text={shortUrl}
+				onCopy={() => toast.success('URL Copied.')}
+			>
+				<button className='flex items-center gap-2 rounded bg-blue-600 text-white py-1 px-2'>
+					<span>Copy url</span> <BiCopy />
+				</button>
+			</CopyToClipboard>
 		</div>
 	)
 }
